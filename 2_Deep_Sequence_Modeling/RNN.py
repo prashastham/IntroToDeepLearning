e331529de7d194a2d65bd719570264c3f3e01a3f
Prashastha Mudannayake <prashasthamudannayake98@gmail.com>
2023-08-19 22:35:24 +0530

    RNNCell class implementation

--- 2_Deep_Sequence_Modeling/RNN.py
+++ 2_Deep_Sequence_Modeling/RNN.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+
 my_rnn = RNN()
 hidden_state = [0, 0, 0, 0]
 
@@ -6,4 +8,27 @@ sentence = ['I', 'love', 'recurrent', 'neural']
 for word in sentence:
     prediction, hidden_state = my_rnn(word, hidden_state)
 
-next_word_prediction = predictionss
+next_word_prediction = prediction
+
+
+class RNNCell(tf.keras.layers.Layer):
+    def __init__(self, rnn_units, input_dim, output_dim):
+        super(RNNCell, self).__init__()
+
+        # Initialize weights and biases
+        self.W_xh = self.add_weight([rnn_units, input_dim])
+        self.W_hh = self.add_weight([rnn_units, rnn_units])
+        self.W_hy = self.add_weight([rnn_units, output_dim])
+
+        # Initialize hidden state to zeros
+        self.h = tf.zeros([rnn_units, 1])
+
+    def call(self, x):
+        # Update hidden state
+        self.h = tf.tanh(tf.matmul(self.h, self.W_hh) + tf.matmul(x, self.W_xh))
+
+        # Compute output
+        ouptut = tf.matmul(self.h, self.W_hy)
+
+        # Return output and hidden state
+        return output, self.h
